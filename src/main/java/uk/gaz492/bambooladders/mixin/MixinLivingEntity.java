64f22eaa1fa913af6aadcfc67a88bae475e2d28f
Gareth Williams <gaz492@gmail.com>
2019-02-17 12:25:27 +0000

    oops

--- src/main/java/uk/gaz492/bambooladders/mixin/MixinLivingEntity.java
+++ src/main/java/uk/gaz492/bambooladders/mixin/MixinLivingEntity.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import uk.gaz492.bambooladders.blocks.BambooLadderBlock;
 import uk.gaz492.bambooladders.util.IClimbable;
 
 @Mixin(LivingEntity.class)
@@ -23,18 +22,4 @@ public abstract class MixinLivingEntity {
             cir.setReturnValue(climbable.canClimb(thisLivingEntity, state, thisLivingEntity.getPos()));
         }
     }
-
-//    @Inject(method = "canClimb", at = @At(value = "RETURN", ordinal = 2), locals = LocalCapture.CAPTURE_FAILHARD, allow = 1, cancellable = true)
-//    void onCanClimb(final CallbackInfoReturnable<Boolean> cir, final BlockState state, final Block block) {
-//        if (block instanceof Climbable) {
-//            final LivingEntity self = (LivingEntity) (Object) this;
-//            cir.setReturnValue(((Climbable) block).canClimb(self, state, getPos()));
-//        } else {
-//            final BlockPos down = new BlockPos(x, y - 0.5, z);
-//            final Block below = world.getBlockState(down).getBlock();
-//            if (below instanceof ClimbableFenceBlock) {
-//                cir.setReturnValue(y - MathHelper.floor(y) < 0.5);
-//            }
-//        }
-//    }
 }
