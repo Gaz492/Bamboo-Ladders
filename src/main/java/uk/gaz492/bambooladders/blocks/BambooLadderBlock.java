5d8e1a1764df83d11558f641607567364529fa05
Gareth Williams <gaz492@gmail.com>
2019-02-17 18:36:21 +0000

    Depend on ladderapi

--- src/main/java/uk/gaz492/bambooladders/blocks/BambooLadderBlock.java
+++ src/main/java/uk/gaz492/bambooladders/blocks/BambooLadderBlock.java
@@ -1,9 +1,8 @@
 package uk.gaz492.bambooladders.blocks;
 
 import net.minecraft.block.LadderBlock;
-import uk.gaz492.bambooladders.util.IClimbable;
 
-public class BambooLadderBlock extends LadderBlock implements IClimbable {
+public class BambooLadderBlock extends LadderBlock {
 
     public BambooLadderBlock(Settings settings) {
         super(settings);
